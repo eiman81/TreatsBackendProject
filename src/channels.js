28edacc68df5b1be91edcaa905f8c42532c7e053
Eiman Zare <eiman.zare@yahoo.com>
2022-06-09 15:24:44 +1000

    Fixed channels merge

--- src/channels.js
+++ src/channels.js
@@ -5,3 +5,7 @@ function channelsCreateV1(authUserId, name, isPublic) {
 function channelsListV1(authUserId) {
     return 'authUserId';
 }
+
+function channelsListallV1(authUserId) {
+  return 'authUserId';
+}
