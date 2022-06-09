182341a37601b6dc1bb6956f68ddd465f3df4bd7
Minjun Kim <z5421922@ad.unsw.edu.au>
2022-06-09 15:41:30 +1000

    created channel join and channel invite

--- src/channel.js
+++ src/channel.js
@@ -2,3 +2,11 @@ function channelDetailsV1(authUserId, channelId) {
   return 'authUserId' + 'channelId';
 }
 
+function channelJoinV1(authUserId, channelId) {
+  return 'authUserId' + 'channelId';
+}
+
+function channelInviteV1(authUserId, channelId, uId) {
+  return 'authUserId' + 'channelId' + 'uId';
+}
+
