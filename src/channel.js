b13516980da7346cac0f7aa2e826f2f70767e890
Sean O'Connor <sean.oconnor1@student.unsw.edu.au>
2022-06-15 13:25:41 +1000

    Update channel.js

--- src/channel.js
+++ src/channel.js
@@ -1,16 +1,26 @@
 function channelDetailsV1(authUserId, channelId) {
-  return 'authUserId' + 'channelId';
+  return {
+    name: 'secret candy crush team', 
+    isPublic: true,
+    ownerMembers: [],
+    allMembers: [],
+  };
 }
 
 function channelJoinV1(authUserId, channelId) {
-  return 'authUserId' + 'channelId';
+  return {};
 }
 
 function channelInviteV1(authUserId, channelId, uId) {
-  return 'authUserId' + 'channelId' + 'uId';
+  return {};
 }
 
 function channelMessagesV1(authUserId, channelId, start) {
-  return 'authUserId' + 'channelId' + 'start';
-} 
+  return {
+    messages: [],
+    start: 0,
+    end: -1,
+  };
+}
 
+export { channelDetailsV1, channelJoinV1, channelInviteV1, channelMessagesV1 };
