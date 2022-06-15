b10cc077d8c10f795d08b503ddfb69b99218acc9
Sean O'Connor <sean.oconnor1@student.unsw.edu.au>
2022-06-15 13:27:23 +1000

    Update channels.js

--- src/channels.js
+++ src/channels.js
@@ -1,11 +1,19 @@
 function channelsCreateV1(authUserId, name, isPublic) {
-    return 'authUserId' + 'name' + 'isPublic';
+  return {
+    channelId: 1,
+  };
 }
 
 function channelsListV1(authUserId) {
-    return 'authUserId';
+  return {
+    channels: [] // see interface for contents
+  };
 }
 
 function channelsListallV1(authUserId) {
-  return 'authUserId';
+  return {
+    channels: [] // see interface for contents
+  };
 }
+
+export { channelsCreateV1, channelsListV1, channelsListallV1 };
