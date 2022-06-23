318c462f98b78183005fbed2465fa7641bb454aa
Dongfu Chen <z5313702@ad.unsw.edu.au>
2022-06-23 17:17:41 +1000

    not finish

--- src/users.js
+++ src/users.js
@@ -1,13 +1,24 @@
 import { getData } from "./dataStore";
 
 function userProfileV1(authUserId, uId) {
-  let store = getData();
-  const userdata = store['user'][uId]
-  if (userdata === undefined) {
-    return {error: 'error'}
+  let valid = 0;
+  for (const user of (getData()).users) {
+    if (user.uId === authUserId) {
+      valid = 1;
+      let profile = {
+        uId: userId,
+        email: email,
+        nameFirst: nameFirst,
+        nameLast: nameLast,
+        username: handle,
+      }
+      return profile;
+    }
   }
-  
-  return {user: uId, user: email, user: nameFirst, user: nameLast};
+  if (valid = 0) {
+    return {error: 'error'}; 
+  }
 }
+
 
 export { userProfileV1 }
