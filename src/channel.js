033de3fd69f49ae5a9c6f7deeaff601218221df8
Dongfu Chen <z5313702@ad.unsw.edu.au>
2022-06-24 16:30:56 +1000

    not finish

--- src/channel.js
+++ src/channel.js
@@ -26,8 +26,13 @@ function channelMessagesV1(authUserId, channelId, start) {
       return {error: 'error'}
     }
 
+  //compare the start+50 and the amount of messages, if start+50 more than amount of messages, the currmessage should be the 
+  //recent message, otherwise, the currmessage should be the start+50
   const currmessage = start + 50 > data.channels.messages.length ? data.channels.messages.length : start + 50;
+  //if this function has returned the least recent messages in the channel(messages.length less than start+50), returns -1 in "end" 
+  //to indicate there are no more messages to load after this return.
   const end = start + 50 > data.channels.messages.length ? -1 : start + 50;
+  //use slice to get the messages between start, start+50, or recent message.
   const reuslt = data.channels.messages.slice(start, currmessage);
   return {messages: reuslt, start: start, end};
   }
