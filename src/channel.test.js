6a56175475cf17aa019d440cde3a1362aa8ac0a5
Aditya <adityamuthukattu@gmail.com>
2022-06-24 16:13:55 +1000

    update to tests channelJOin and Invite

--- src/channel.test.js
+++ src/channel.test.js
@@ -25,41 +25,100 @@ user with PermissionStatus, private channel
 
 import { getData, setData } from "./dataStore.js";
 import { channelInviteV1, channelJoinV1, channelsCreateV1 } from './channel';
-import { authRegisterV1, authLoginV1 } from './auth';
+import { authRegisterV1 } from './auth';
 import { clearV1 } from './other';
-
+/*
 // Setup
 // create first valid user
+function initialize(){
+    let count = 1
+    const validuser1  = {
+        uId: 1,
+        nameFirst: 'Christiano',
+        nameLast: 'Ronaldo',
+        email: 'cristiano.ronaldo@unsw.edu.au',
+        password: '123456',
+        username: 'christianosui',
+        userRole: null,
+        isOnline: null,
+      }
+    
+      count += 1;
+    //create second valid user
+    const validuser2   = {
+        uId: count,
+        nameFirst: 'Mohammed',
+        nameLast: 'MayweatherJr',
+        email: 'mohammed.mayweatherjr@unsw.edu.au',
+        password: 'notfloyd',
+        username: 'realmayweather',
+        userRole: null,
+        isOnline: null,
+      }
+    count += 1;
+    
+    //create second valid user
+    const validchannel = {
+        channelId: count,
+        channelName: 'Discussion',
+        latestMsg: 'Hi Everyone!',
+        isPublic: 'true',
+        password: null,//'comp1531' //empty if 'isPublic' is true
+        ownerMembers: [validuser1],
+        allMembers: [validuser1],
+    }
+    count += 1;
+    
+    //create invalid users
+    const invaliduser = {
+        uId: 6789,
+        nameFirst: 'Post',
+        nameLast: 'Malone',
+        email: 'post.malone@unsw.edu.au',
+        password: 'stoney',
+        username: 'leondechino',
+        userRole: null,
+        isOnline: null,
+      }
+    
+      const invalidchannel = {
+        channelId: 7890,
+        channelName: 'Argument',
+        latestMsg: 'Shut Up!!',
+        isPublic: 'true',
+        password: null,//'comp1531' //empty if 'isPublic' is true
+        ownerMembers: [],
+        allMembers: [],
+    }
+    
+    const privatechannel = {
+        channelId: count,
+        channelName: 'floyds priv channel',
+        latestMsg: 'sup',
+        isPublic: 'false',
+        password: 'floyd',//'comp1531' //empty if 'isPublic' is true
+        ownerMembers: [validuser2],
+        allMembers: [validuser2],
+    }
+    
+    let data = {
+        users: [validuser1, validuser2],
+        channels: [validchannel, privatechannel],
+    }
+    setData(data);
+}
+*/
+/*
+let store = getData()
+let users = store.users
+let channels = store.channels
+*/
 
-let count = 1
-const validuser1  = {
-    uId: 1,
-    nameFirst: 'Christiano',
-    nameLast: 'Ronaldo',
-    email: 'cristiano.ronaldo@unsw.edu.au',
-    password: '123456',
-    username: 'christianosui',
-    userRole: null,
-    isOnline: null,
-  }
+const validuser1 = authRegisterV1('another_cristiano@unsw.edu.au', '123456', 'Cristiano', 'Ronaldo');
+const validuser2 = authRegisterV1('mohammed.mayweatherjr@unsw.edu.au', 'notfloyd', 'Mohammed', 'MayweatherJr');
 
-  count += 1;
-//create second valid user
-const validuser2   = {
-    uId: count,
-    nameFirst: 'Mohammed',
-    nameLast: 'MayweatherJr',
-    email: 'mohammed.mayweatherjr@unsw.edu.au',
-    password: 'notfloyd',
-    username: 'realmayweather',
-    userRole: null,
-    isOnline: null,
-  }
-count += 1;
-
-//create second valid user
 const validchannel = {
-    channelId: count,
+    channelId: 3,
     channelName: 'Discussion',
     latestMsg: 'Hi Everyone!',
     isPublic: 'true',
@@ -67,7 +126,7 @@ const validchannel = {
     ownerMembers: [validuser1],
     allMembers: [validuser1],
 }
-count += 1;
+
 
 //create invalid users
 const invaliduser = {
@@ -92,7 +151,7 @@ const invaliduser = {
 }
 
 const privatechannel = {
-    channelId: count,
+    channelId: 4,
     channelName: 'floyds priv channel',
     latestMsg: 'sup',
     isPublic: 'false',
@@ -101,50 +160,29 @@ const privatechannel = {
     allMembers: [validuser2],
 }
 
+
 let data = {
     users: [validuser1, validuser2],
     channels: [validchannel, privatechannel],
 }
 setData(data);
-/*
-let store = getData()
-let users = store.users
-let channels = store.channels
-*/
-// testing
-test('channelJoinV1: valid user (who is not already in channel), valid channel', () => {
-  clearV1();
-  
-  let result = channelJoinV1(validuser2.uId, validchannel.channelId)
-  
-  expect(result).toStrictEqual({});
+
+const cases = [[validuser2, validchannel.channelId, {}], 
+               [invaliduser.uId, validchannel.channelId, {error: 'error'}], 
+               [validuser2, invalidchannel.channelId, {error: 'error'}], 
+               [invaliduser.uId, invalidchannel.channelId,{error: 'error'}]];
+
+describe("'channelJoinV1' utility", () => {
+  test.each(cases)(
+    "given user %p and channel %p as arguments, returns %p",
+    (firstArg, secondArg, expectedResult) => {
+      const result = channelJoinV1(firstArg, secondArg);
+      expect(result).toStrictEqual(expectedResult);
+    }
+  );
 });
 
 
-test('channelJoinV1: invalid user, valid channel', () => {
-    clearV1();
-    
-    let result = channelJoinV1(invaliduser.uId, validchannel.channelId)
-    
-    expect(result).toStrictEqual({error: 'error'});
-  });
-  
-
-test('channelJoinV1: valid user, invalid channel', () => {
-    clearV1();
-    
-    let result = channelJoinV1(validuser2.uId, invalidchannel.channelId)
-    
-    expect(result).toStrictEqual({error: 'error'});
-  });
-
-test('channelJoinV1: invalid user, invalid channel', () => {
-    clearV1();
-    
-    let result = channelJoinV1(invaliduser.uId, invalidchannel.channelId)
-    
-    expect(result).toStrictEqual({error: 'error'});
-  });
 
 test('channelJoinV1: authorised user already a member of channel', () => {
     clearV1();
@@ -192,4 +230,3 @@ test('channelInviteV1: invalid authuserId, valid uId, valid channel', () => {
     
     expect(result).toStrictEqual({error: 'error'});
   });
-  
