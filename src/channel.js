2b37326d843d90f45f13989dd5e87544fc5b1e8c
Dongfu Chen <z5313702@ad.unsw.edu.au>
2022-06-24 15:38:53 +1000

    not finish

--- src/channel.js
+++ src/channel.js
@@ -18,18 +18,19 @@ function channelInviteV1(authUserId, channelId, uId) {
 }
 
 function channelMessagesV1(authUserId, channelId, start) {
-  let store = getData();
-  const channeldata = store['channel'][channelId]
-  if (channeldata === NaN || start > channeldata['messages'].length || ! authUserId in channeldata['members']) {
-    return {error: 'error'}
-  }
-  
-  const currmessage = start + 50 > channeldata['messages'].length ? channeldata['messages'].length : start + 50;
-  const end = start + 50 > channeldata['messages'].length ? -1 : start + 50;
-  // get the messages between index "start" and "start + 50"
-  const reuslt = channeldata.messages.slice(start, currmessage);
+  const data = getData();
+  if (authUserId in data.users && channelId in data.channels){
+    if (!(authUserId in channelDetailsV1(authUserId, channelId).allMembers)){
+      return {error: 'error'}
+    }else if (start > data.channels.messages.length) {
+      return {error: 'error'}
+    }
 
-  return {messages: reuslt, start: start, end: end};
+  const currmessage = start + 50 > data.channels.messages.length ? data.channels.messages.length : start + 50;
+  const end = start + 50 > data.channels.messages.length ? -1 : start + 50;
+  const reuslt = data.channels.messages.slice(start, currmessage);
+  return {messages: reuslt, start: start, end};
+  }
 }
 
 export { channelDetailsV1, channelJoinV1, channelInviteV1, channelMessagesV1 };
