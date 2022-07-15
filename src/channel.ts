cc8760ea0702f459fde3214889a8fc47e6e95011
Aditya Muthukattu <a.muthukattu@student.unsw.edu.au>
2022-07-15 19:17:33 +1000

    implemented channel Leave function

--- src/channel.ts
+++ src/channel.ts
@@ -273,4 +273,42 @@ Return Value:
   }
 }
 
-export { channelDetailsV1, channelJoinV1, channelInviteV1, channelMessagesV1 };
+function channelLeaveV1(token: string, channelId: number): {error: 'error'} | {} {
+
+  const store = getData();
+  let authUserId: number;
+  for (const user of store.users) {
+    if (user.token === token) {
+      authUserId = user.uId;
+      break;
+    }
+  }
+  const profile = userProfileV1(token, authUserId);
+
+
+  if ('error' in profile) {
+    return { error: 'error' };
+  } else {
+    if (userExists(authUserId) && channelExists(channelId)) {
+      const channelDetails = channelDetailsV1(token, channelId);
+      if ('allMembers' in channelDetails) {
+        if (channelDetails.allMembers.includes(authUserId)) {
+          let counter = 0;
+          for (const channel of getData().channels) {
+            if (channel.channelId === channelId) {
+              channel.allMembers.splice(authUserId);
+              store.channels[counter] = channel;
+              setData(store);
+              return {};
+            }
+            counter++;
+          }
+        } else {
+          return { error: 'error' };
+        }
+      }
+    }
+  }
+}
+
+export { channelDetailsV1, channelJoinV1, channelInviteV1, channelMessagesV1, channelLeaveV1 };
