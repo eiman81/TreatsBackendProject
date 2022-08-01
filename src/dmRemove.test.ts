7912f6b3572437bcc0c0554da11d4fc3231a5028
Aditya <adityamuthukattu@gmail.com>
2022-08-02 02:32:28 +1000

    tests for dm Create, Details, Leave, Remove

--- src/dmRemove.test.ts
+++ src/dmRemove.test.ts
@@ -1,8 +1,8 @@
 import { dmDetailsV1 } from './directMessages';
-import { clearV1, authRegisterV1, dmCreateV1 } from "./httpWrappers";
+import { clearV1, authRegisterV1, dmCreateV1, dmRemoveV1 } from "./httpWrappers";
 import { authUserId } from './auth';
 
-test('dmCreateV1: succcess', () => {
+test('dmRemoveV1: succcess', () => {
   clearV1();
   const a = authRegisterV1('cristiano.ronaldo@unsw.edu.au', '123456', 'Cristiano', 'Ronaldo') as authUserId;
   const b = authRegisterV1('another_cristiano@unsw.edu.au', 'cristiano7', 'Cristiano', 'Ronaldo') as authUserId;;
@@ -10,10 +10,10 @@ test('dmCreateV1: succcess', () => {
   const d = authRegisterV1('zero0@unsw.edu.au', '12345678', 'ZERO', '0') as authUserId;
   
   const group = dmCreateV1(a.token, [a.authUserId, b.authUserId, c.authUserId, d.authUserId]) as dmId;
-  expect(group).toStrictEqual({ dmId });
+  expect(dmRemoveV1(a.token, group.dmId)).toStrictEqual({});
 });
 
-test('dmCreateV1: error for duplicate uId', () => {
+test('dmRemoveV1: error for invalid dmId', () => {
     clearV1();
     const a = authRegisterV1('cristiano.ronaldo@unsw.edu.au', '123456', 'Cristiano', 'Ronaldo') as authUserId;
     const b = authRegisterV1('another_cristiano@unsw.edu.au', 'cristiano7', 'Cristiano', 'Ronaldo') as authUserId;;
@@ -21,16 +21,27 @@ test('dmCreateV1: error for duplicate uId', () => {
 
     
     const group = dmCreateV1(a.token, [a.authUserId, b.authUserId, c.authUserId, c.authUserId]);
-    expect(group).toStrictEqual({ error: 'error' });
+    expect(dmRemoveV1(a.token, 5832)).toStrictEqual({ error: 'error' });
   });
 
-  test('dmCreateV1: error for invalid uID', () => {
+  test('dmRemoveV1: error for authuserId not creator', () => {
     clearV1();
     const a = authRegisterV1('cristiano.ronaldo@unsw.edu.au', '123456', 'Cristiano', 'Ronaldo') as authUserId;
     const b = authRegisterV1('another_cristiano@unsw.edu.au', 'cristiano7', 'Cristiano', 'Ronaldo') as authUserId;;
     const c = authRegisterV1('mohammed.mayweatherjr@unsw.edu.au', 'notfloyd', 'Mohammed', 'MayweatherJr') as authUserId;;
-
+    const d = authRegisterV1('zero0@unsw.edu.au', '12345678', 'ZERO', '0') as authUserId;
     
-    const group = dmCreateV1(a.token, [a.authUserId, b.authUserId, c.authUserId, 9871]);
-    expect(group).toStrictEqual({ error: 'error' });
+    const group = dmCreateV1(a.token, [a.authUserId, b.authUserId, c.authUserId, d.authUserId]) as dmId;
+    expect(dmRemoveV1(d.token, group.dmId)).toStrictEqual({ error: 'error' });
+  });
+
+  test('dmRemoveV1: error for authuserId not in dm', () => {
+    clearV1();
+    const a = authRegisterV1('cristiano.ronaldo@unsw.edu.au', '123456', 'Cristiano', 'Ronaldo') as authUserId;
+    const b = authRegisterV1('another_cristiano@unsw.edu.au', 'cristiano7', 'Cristiano', 'Ronaldo') as authUserId;;
+    const c = authRegisterV1('mohammed.mayweatherjr@unsw.edu.au', 'notfloyd', 'Mohammed', 'MayweatherJr') as authUserId;;
+    const d = authRegisterV1('zero0@unsw.edu.au', '12345678', 'ZERO', '0') as authUserId;
+    
+    const group = dmCreateV1(a.token, [a.authUserId, b.authUserId, c.authUserId]) as dmId;
+    expect(dmRemoveV1(d.token, group.dmId)).toStrictEqual({ error: 'error' });
   });
